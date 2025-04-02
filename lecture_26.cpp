3abcb73ae79dbedd78dec9b3f1ae9b50c6df38a1
lovkumawat <kumawatlov6@gmail.com>
2025-04-03 01:02:05 +0530

    Revisiting Pointers commite successfully

---
+++ lecture_26.cpp
@@ -0,0 +1,37 @@
+// Revisiting Pointers: new and delete Keywords in CPP
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Basic Example 
+    int a=4;
+    int *ptr=&a;
+
+    cout<<"The value of a "<<*(ptr)<<endl;
+    // new Keyword
+    int *p=new int(40);
+    cout<<"The adderss is :"<<p<<endl;
+    cout<<"The value at addreass p is "<<*(p)<<endl;
+
+    int *arr=new int[3];
+    arr[0]=10;
+    *(arr+1)=20;
+    arr[2]=30;
+    cout<<"The value of arr[0] is "<<arr[0]<<endl;
+    cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    cout<<"The value of arr[2] is "<<arr[2]<<endl;
+
+    delete arr;
+    cout<<"The value of arr[0] is "<<arr[0]<<endl;
+    cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    cout<<"The value of arr[2] is "<<arr[2]<<endl;
+    
+    delete[] arr;
+    cout<<"The value of arr[0] is "<<arr[0]<<endl;
+    cout<<"The value of arr[1] is "<<arr[1]<<endl;
+    cout<<"The value of arr[2] is "<<arr[2]<<endl;
+    
+
+    // 
+    return 0;
+}
